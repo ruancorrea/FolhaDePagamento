dbd5a7ac7bc8716b0d216f10d197b17331758bbc
RCorrea <47988061+ruancorrea@users.noreply.github.com>
2019-06-25 20:32:22 -0300

    Create Assalariado.java

---
+++ ProjetoOO/Assalariado.java
@@ -0,0 +1,10 @@
+package P_OO;
+
+public class Assalariado extends Funcionario{
+
+    public Assalariado(String nome, String endereco, int ID, String Sindicato, String SindicatoID, double salario, double taxaSindical, String tipo,
+                       String metodo, String pagamento, String agenda, String nasemana, int day, int month, int year, int diastrabalhados,
+                       double salarioAtual, boolean cartao, int diaspassados, boolean taxa, boolean taxa2) {
+        super(nome, endereco, ID, Sindicato, SindicatoID, taxaSindical, tipo, salario, metodo, pagamento, agenda, nasemana, day,month,year,diastrabalhados,salarioAtual, cartao, diaspassados, taxa, taxa2);
+    }
+}
