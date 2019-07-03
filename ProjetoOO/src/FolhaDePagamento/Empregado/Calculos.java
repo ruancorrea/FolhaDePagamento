a529342e0a533455fe2b615a765cd973eda170a2
RCorrea <47988061+ruancorrea@users.noreply.github.com>
2019-07-03 12:14:53 -0300

    Delete Calculos.java

--- ProjetoOO/src/FolhaDePagamento/Empregado/Calculos.java
+++
@@ -1,9 +0,0 @@
-package FolhaDePagamento.Empregado;
-
-import FolhaDePagamento.Administrador.Empresa;
-
-public interface Calculos {
-    public void CalculoSalario(Funcionario F);
-
-    public String CalcularDiaPagamento(Empresa P3, Funcionario F, int n);
-}
